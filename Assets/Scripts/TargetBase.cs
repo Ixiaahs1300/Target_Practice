0750e6ce778ae9465e26efff0599e280b33d3a49
Ixiaahs1300 <isaiahepoole13@yahoo.com>
2021-08-11 21:06:37 -0400

    Player prefab and target behaviors
    Added base script for blink target and added the player as a prefab to the prefabs folder

--- Assets/Scripts/TargetBase.cs
+++ Assets/Scripts/TargetBase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetBehavior : MonoBehaviour
+public class TargetBase : MonoBehaviour
 {
 
     private void OnCollisionEnter(Collision collision)
---
+++ Assets/Scripts/TargetBlink.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBlink : TargetBase
+{
+    MeshRenderer mr;
+    [SerializeField] Material red;
+    [SerializeField] Material blue;
+    Material currentMat;
+    [SerializeField] float switchTime = 3f;
+
+    void Awake()
+    {
+        mr = GetComponent<MeshRenderer>();
+        currentMat = red;
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("ChangeType", switchTime, switchTime);
+    }
+
+    void ChangeType()
+    {
+        if (currentMat.Equals(blue))
+        {
+            mr.materials[0] = red;
+        }
+        else if(currentMat.Equals(red))
+        {
+            mr.materials[0] = blue;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*if(currentMat.Equals(red))
+        {
+
+        }
+        else if(currentMat.Equals(blue))
+        {
+
+        }*/
+    }
+}
