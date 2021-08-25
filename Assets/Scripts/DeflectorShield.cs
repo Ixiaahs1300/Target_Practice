531bb123851dac208d6f093ef7d310e2c523a32d
Ixiaahs1300 <isaiahepoole13@yahoo.com>
2021-08-24 22:35:25 -0400

    Implemented Deflector Shield
    Implemented the deflector shield mechanic. Shield can be activated by button press, and can deflect projectiles that are shot at it

---
+++ Assets/Scripts/DeflectorShield.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeflectorShield : MonoBehaviour
+{
+    [SerializeField] KeyCode shieldKey = KeyCode.Mouse1;
+    [SerializeField] GameObject shield;
+    private float maxShieldSize;
+    [SerializeField] private float shieldShrinkTime = 1f;
+    [SerializeField] private float shieldGrowTime = 1f;
+    private Vector3 shieldShrinkVelocity = new Vector3(0f, 0f, 0f);
+    private Vector3 shieldGrowVelocity = new Vector3(0f, 0f, 0f);
+    private bool isShieldShrinking;
+    [SerializeField] private float deflectForce = 30f;
+    Vector3 relVel;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        isShieldShrinking = false;
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (shield.activeInHierarchy && other.gameObject.tag.Equals("Projectile"))
+        {
+            //other.transform.GetComponent<Rigidbody>().AddForce(transform.forward * deflectForce, ForceMode.Impulse);
+            Transform otherTrans = other.transform;
+            otherTrans.GetComponent<Rigidbody>().AddForce(otherTrans.forward * deflectForce * -1, ForceMode.Impulse);
+        }
+    }
+    
+
+    void ActivateShield()
+    {
+        if(Input.GetKey(shieldKey))
+        {
+            isShieldShrinking = true;
+            shield.SetActive(true);
+        }
+        else
+        {
+            isShieldShrinking = false;
+            shield.SetActive(false);
+        }
+    }
+
+    IEnumerator ShrinkShield()
+    {
+        while (shield.transform.localScale.y >= 0 && shield.transform.localScale.z >= 0)
+        {
+            if (isShieldShrinking)
+            {
+                shield.transform.localScale = Vector3.SmoothDamp(shield.transform.localScale, new Vector3(shield.transform.localScale.x, shield.transform.localScale.y - 0.1f, shield.transform.localScale.z - 0.1f), ref shieldShrinkVelocity, shieldShrinkTime);
+                if(shield.transform.localScale.y < 0 || shield.transform.localScale.z < 0)
+                {
+                    shield.transform.localScale = new Vector3(shield.transform.localScale.x, 0f, 0f);
+                }
+                yield return new WaitForSeconds(0.1f);
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+    }
+
+    IEnumerator GrowShield()
+    {
+        while (shield.transform.localScale.y <= 0.75f && shield.transform.localScale.z <= 0.75f)
+        {
+            if (!isShieldShrinking)
+            {
+                //energyShield.transform.localScale = Vector3.Lerp(energyShield.transform.localScale, new Vector3(shieldScale, shieldScale, 1f), 1000f);
+                shield.transform.localScale = Vector3.SmoothDamp(shield.transform.localScale, new Vector3(shield.transform.localScale.x, shield.transform.localScale.y + 0.05f, shield.transform.localScale.z + 0.05f), ref shieldGrowVelocity, shieldGrowTime);
+                if (shield.transform.localScale.y > 0.75f || shield.transform.localScale.z > 0.75f)
+                {
+                    shield.transform.localScale = new Vector3(shield.transform.localScale.x, 0.75f, 0.75f);
+                }
+                yield return new WaitForSeconds(0.1f);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ActivateShield();
+        StartCoroutine(ShrinkShield());
+        StartCoroutine(GrowShield());
+    }
+}
---
+++ Assets/Scripts/PitchingMachine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitchingMachine : MonoBehaviour
+{
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private Transform player;
+    [SerializeField] private float shootForce = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("PitchProjectile");
+    }
+
+    IEnumerator PitchProjectile()
+    {
+        while(true)
+        {
+            GameObject proj = Instantiate(projectile);
+            //proj.transform.SetParent(transform);
+            proj.transform.position = transform.position + transform.forward;
+            proj.transform.LookAt(player);
+            proj.GetComponent<Rigidbody>().freezeRotation = true;
+            proj.GetComponent<Rigidbody>().AddForce((player.position - proj.transform.position).normalized * shootForce, ForceMode.Impulse);
+            yield return new WaitForSeconds(2f);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
